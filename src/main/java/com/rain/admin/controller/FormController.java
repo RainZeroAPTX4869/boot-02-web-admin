6e6fabedd464b40ffe0d825b1cb48e46b8425706
rain <2368001047@qq.com>
2021-03-04 16:14:16 +0800

    16:14

--- src/main/java/com/rain/admin/controller/FormController.java
+++ src/main/java/com/rain/admin/controller/FormController.java
@@ -2,6 +2,13 @@ package com.rain.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,8 +18,26 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class FormController {
+
     @GetMapping("form_layouts")
     public String formLayout() {
+        return "form/form_layouts";
+    }
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("email") String email,
+                         @RequestParam("username") String username,
+                         @RequestPart("headImg") MultipartFile[] headImgs) throws IOException {
+        if (headImgs.length > 0) {
+            for (MultipartFile headImg : headImgs) {
+                if (!headImg.isEmpty()) {
+                    String filename = headImg.getOriginalFilename();
+                    headImg.transferTo(new File("C:\\" + filename));
+                }
+            }
+
+        }
+
         return "form/form_layouts";
     }
 
