4df237834966c358345cf8b083e38dd28be4bb59
rain <2368001047@qq.com>
2021-03-19 17:21:36 +0800

    17:21

--- src/main/java/com/rain/admin/controller/BaseStoreMacController.java
+++ src/main/java/com/rain/admin/controller/BaseStoreMacController.java
@@ -1,11 +1,14 @@
 package com.rain.admin.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rain.admin.bean.BaseStoreMac;
 import com.rain.admin.service.BaseStoreMacService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 门店MAC地址(BaseStoreMac)表控制层
@@ -18,8 +21,10 @@ public class BaseStoreMacController {
     /**
      * 服务对象
      */
-    @Resource
+    @Autowired
     private BaseStoreMacService basestoremacService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     /**
      * 通过主键查询单条数据
@@ -29,8 +34,17 @@ public class BaseStoreMacController {
      */
     @ResponseBody
     @GetMapping("selectOne")
-    public BaseStoreMac selectOne(@RequestParam("id") String id) {
-        return this.basestoremacService.queryById(id);
+    public String selectOne(@RequestParam("id") String id) throws JsonProcessingException {
+        BaseStoreMac baseStoreMac = this.basestoremacService.queryById(id);
+        return objectMapper.writeValueAsString(baseStoreMac);
+
+    }
+
+    @ResponseBody
+    @GetMapping("select")
+    public String select(@RequestParam("offset") int offset, @RequestParam("limit") int limit) throws JsonProcessingException {
+        List<BaseStoreMac> baseStoreMacs = this.basestoremacService.queryAllByLimit(offset, limit);
+        return objectMapper.writeValueAsString(baseStoreMacs);
     }
 
 }
